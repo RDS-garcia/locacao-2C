f41ea0f78ea1369476900c9c988a144b4bbf2e44
gustavoweb <contato@gustavoweb.com>
2025-05-26 09:23:40 -0300

    Add files via upload

--- backend/app.js
+++ backend/app.js
@@ -7,17 +7,3 @@ const app = express();
 const server = http.createServer(app);
 
 //Conexões com BD e Escutar Servidor
-const inicializar = async () => {
-
-    //Escutar o Servidor
-    const PORT = process.env.PORT || 3000;
-    server.listen(PORT, () => {
-        console.log(`Servidor Rodando na Porta ${PORT}`);
-    });
-};
-
-//Chamada da função
-inicializar();
-
-//exportar os modulos para reutilizar em outro arquivo
-module.exports = {app, server};
